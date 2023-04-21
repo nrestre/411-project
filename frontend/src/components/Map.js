6f1e324194ece577de4d8e7073f225cf79a083f7
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2023-04-20 23:40:33 -0400

    Added front end as new folder (#17)
    Co-authored-by: NicolasRestrepoG <nicolasrestrepog2506@gmail.com>

---
+++ frontend/src/components/Map.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import requests from '../requests';
+
+const Map = () => {
+    const mapContainerStyle = {
+        height: "400px",
+        width: "100%",
+    };
+
+    const center = {
+        lat: 37.7749,
+        lng: -122.4194,
+    };
+
+    return (
+        <LoadScript
+            //Replace with Http request to backend
+            googleMapsApiKey="YOUR_API_KEY"
+            //fetchMap()
+        >
+            <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                center={center}
+                zoom={10}           //Modify to appropriate
+            >
+                {/* Map content */}
+            </GoogleMap>
+        </LoadScript>);
+};
+
+export default Map;
