3844ea6064f8263279e92c690c5930c5e996ab5a
NicolasRestrepoG <nicolasrestrepog2506@gmail.com>
2023-04-23 14:31:08 -0400

    Interface fixed, working on google maps

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['https://brandlogovector.com/wp-content/uploads/2021/03/Boston-University-Logo-Icon.png',
+      'https://w1.pngwing.com/pngs/80/795/png-transparent-school-logo-boston-university-school-academic-degree-postdoctoral-researcher-language-school-collegexpress-text.png'],
+  },
   experimental: {
     appDir: true,
   },
