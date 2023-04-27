b8a69b8e7d9a413244c62846c3086215798024ef
aaron-ang <67321817+aaron-ang@users.noreply.github.com>
2023-04-26 23:33:43 -0400

    rm `frontend` dir, adjust styling

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,50 +1,12 @@
-<<<<<<< HEAD
-=======
-import React from "react";
->>>>>>> 3844ea6 (Interface fixed, working on google maps)
 import "./sidebar.css";
 
 const SideBar = () => {
   return (
-<<<<<<< HEAD
     <div className="sidebar-container">
       <ul className="list">
         <li className="option">Option 1</li>
         <li className="option">Option 2</li>
         <li className="option">Option 3</li>
-=======
-    <div style={{ width: "200px", backgroundColor: "#f0f0f0" }}>
-      <ul style={{ listStyle: "none", padding: 0, marginTop: "1rem" }}>
-        <li
-          style={{
-            padding: "8px",
-            cursor: "pointer",
-            backgroundColor: "#fff",
-            borderBottom: "1px solid #ddd",
-          }}
-        >
-          Option 1
-        </li>
-        <li
-          style={{
-            padding: "8px",
-            cursor: "pointer",
-            backgroundColor: "#fff",
-            borderBottom: "1px solid #ddd",
-          }}
-        >
-          Option 2
-        </li>
-        <li
-          style={{
-            padding: "8px",
-            cursor: "pointer",
-            backgroundColor: "#fff",
-          }}
-        >
-          Option 3
-        </li>
->>>>>>> 3844ea6 (Interface fixed, working on google maps)
       </ul>
     </div>
   );
--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -13,15 +13,9 @@ const Weather = () => {
   });
   if (error) return notFound();
   return (
-<<<<<<< HEAD
     <div className="weather-container">
       <h1>Current Weather</h1>
       <h2>Boston University</h2>
-=======
-    <>
-      <h1 className="header-weather" >Current Weather</h1>
-      <h2 className="location" >Boston University</h2>
->>>>>>> 3844ea6 (Interface fixed, working on google maps)
       {isLoading ? (
         <SyncLoader
           color="#36d7b7"
@@ -30,8 +24,8 @@ const Weather = () => {
         />
       ) : (
         <>
-          <div className="temperature" >Temp: {data.main.temp} F</div>
-          <div className="feel-like" >Feels like: {data.main.feels_like} F</div>
+          <div>Temp: {data.main.temp} F</div>
+          <div>Feels like: {data.main.feels_like} F</div>
         </>
       )}
     </div>
