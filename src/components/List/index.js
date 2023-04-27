c1bc98a30b57a9fff0cfcbd3c296571b1848165c
Ian <iant@bu.edu>
2023-04-27 14:28:59 -0400

    render location list

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,24 +1,10 @@
-"use client";
-
-import React, { useState, useEffect } from "react";
-
-export default function List() {
-  const [data, setData] = useState([]);
-
-  /*useEffect(() => {
-        // Fetch data from the database
-       fetchLocations()
-            .then((response) => response.json())
-            .then((data) => setData(data));
-    }, []);*/
-
+export default function List({locations}) {
   return (
     <div>
-      {/* Render the list using the data */}
-      {data.map((item) => (
-        <div key={item.id}>
-          {/* Render list item */}
-          <p>{item.name}</p>
+      <h1>Study Location List</h1>
+      {locations.map((location) => (
+        <div key={location.id}>
+          <p>{location.name}</p>
         </div>
       ))}
     </div>
