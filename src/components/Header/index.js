cd2332cac5ce0cb10efb415122a6aa3229d9d42c
NicolasRestrepoG <nicolasrestrepog2506@gmail.com>
2023-04-30 19:55:17 -0400

    Done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,19 +11,21 @@ const Header = () => {
     const [ratio, setRatio] = useState(16 / 9);
     return (
         <header className={styles.headerContainer}>
-            <Image
-                src="/logo.png"
-                alt="Logo"
-                width={100}
-                height={100 / ratio}
-                className={styles.logoImg}
-                onLoadingComplete={({ naturalWidth, naturalHeight }) =>
-                    setRatio(naturalWidth / naturalHeight)
-                }
-            />
-            <Link href="/dashboard">
-                <h1 className={styles.title}>Study Buddy</h1>
-            </Link>
+            <div className={styles.titleLogo}>
+                <Image
+                    src="/logo.png"
+                    alt="Logo"
+                    width={100}
+                    height={100 / ratio}
+                    className={styles.logoImg}
+                    onLoadingComplete={({ naturalWidth, naturalHeight }) =>
+                        setRatio(naturalWidth / naturalHeight)
+                    }
+                />
+                <Link href="/dashboard">
+                    <h1 className={styles.title}>Study Buddy</h1>
+                </Link>
+            </div>
             <SignOutButton />
         </header>
     );
